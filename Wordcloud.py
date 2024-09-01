d069bc96299edbb53f9b8b03de76bf89da5038e1
Akihiro-hub <tsukamoto.akihiro@kmcinc.co.jp>
2024-09-02 07:06:39 +0900

    Update Wordcloud.py

--- Wordcloud.py
+++ Wordcloud.py
@@ -33,7 +33,7 @@ default_excluded_words = {
     "la", "el", "los", "las", "él", "ella", "en", "de", "del", "un", "que", "soy", "eres", "es", "somos", "son",
     "estoy", "estás", "le", "poder", "hace", "año", "mes", "he", "estado", "había", "años", "meses",
     "está", "estamos", "están", "este", "aquello", "aquella", "esta", "estas", "estos", "cual", "y", "ya", "hay", "a", "al",
-    "lo", "desde", "hasta", "hacia", "usted", "tú", "yo", "compartir",
+    "lo", "desde", "hasta", "hacia", "usted", "tú", "yo", "compartir", "con", "para", "su", "nuestro",
     "o", "u", "e", "por", "eso", "foto", "fotos", "local", "reseñas", "más", "mas", "nos", "os", "ser", "estar", "sí", "si",
     "no", "ni", "guide", "hay", "se", "una", "uno"
 }
