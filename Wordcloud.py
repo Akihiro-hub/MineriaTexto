98f00882ce037387e2dac2571173a89db31e5253
Akihiro-hub <tsukamoto.akihiro@kmcinc.co.jp>
2024-09-02 06:59:49 +0900

    Update Wordcloud.py

--- Wordcloud.py
+++ Wordcloud.py
@@ -30,7 +30,7 @@ with col3:
 
 # デフォルトで除外する単語のリスト
 default_excluded_words = {
-    "la", "el", "los, "las", "él", "ella", "en", "de", "del", "un", "que", "soy", "eres", "es", "somos", "son",
+    "la", "el", "los", "las", "él", "ella", "en", "de", "del", "un", "que", "soy", "eres", "es", "somos", "son",
     "estoy", "estás", "le", "poder", "hace", "año", "mes", "he", "estado", "había", "años", "meses",
     "está", "estamos", "están", "este", "aquello", "aquella", "esta", "estas", "estos", "cual", "y", "ya", "hay", "a", "al",
     "lo", "desde", "hasta", "hacia", "usted", "tú", "yo",
