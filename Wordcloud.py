4a0d3a267203f5cb71f87e0e3acdbf548ebbcfe7
Akihiro-hub <tsukamoto.akihiro@kmcinc.co.jp>
2024-09-02 06:47:47 +0900

    Update Wordcloud.py

--- Wordcloud.py
+++ Wordcloud.py
@@ -32,7 +32,7 @@ with col3:
 default_excluded_words = {
     "la", "el", "en", "de", "del", "un", "que", "soy", "eres", "es", "somos", "son", "estoy", "estás", "le",
     "está", "estamos", "están", "este", "aquello", "aquella", "esta", "estas", "estos", "cual", "y", "ya",
-    "o", "u", "e", "por", "eso", "foto", "fotos", "local", "reseñas", "más", "mas", "nos", "os", "ser", "estar", "sí", "si", "no", "ni", "guide", "hay"m "se"m "una", "uno"
+    "o", "u", "e", "por", "eso", "foto", "fotos", "local", "reseñas", "más", "mas", "nos", "os", "ser", "estar", "sí", "si", "no", "ni", "guide", "hay", "se", "una", "uno"
 }
 
 # 入力された除外単語をセットに追加
