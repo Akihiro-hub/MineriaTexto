c254818689ba16045dff1973a64dc1b95d497d23
Akihiro-hub <tsukamoto.akihiro@kmcinc.co.jp>
2024-09-02 17:14:03 +0900

    Update Wordcloud.py

--- Wordcloud.py
+++ Wordcloud.py
@@ -14,9 +14,9 @@ st.write("##### :green[Paso 1: Pegue el texto para el análisis o suba un archiv
 # テキストエリアとPDFファイルアップロードのオプション
 col1, col2 = st.columns(2)
 with col1:
-    texto = st.text_area("Introduce el texto aquí", "")
+    texto = st.text_area("Pegue aquí el texto a analizar", "")
 with col2: 
-    pdf_file = st.file_uploader("O sube un archivo PDF", type="pdf")
+    pdf_file = st.file_uploader("O sube un archivo PDF a analizar", type="pdf")
 
 # 除外したい単語の入力
 st.write("##### :green[Paso 2: Note abajo las palabras que deben excluirse del análisis, si las tiene.]")
