3cd073734edd5b64591ecbf55f9e4c7c4813d5af
Akihiro-hub <tsukamoto.akihiro@kmcinc.co.jp>
2024-09-16 23:05:26 +0900

    Update Wordcloud.py

--- Wordcloud.py
+++ Wordcloud.py
@@ -18,6 +18,7 @@ with col1:
     texto = st.text_area("Pegue aquí el texto a analizar", "")
 with col2: 
     pdf_file = st.file_uploader("O, sube un archivo PDF a analizar, como segunda opción", type="pdf")
+st.write("Esta aplicación se mantiene durante el período del Proyecto GuateCrece.")
 
 # 除外したい単語の入力
 st.write("##### :green[Paso 2: Note abajo las palabras que deben excluirse del análisis, si las tiene.]")
